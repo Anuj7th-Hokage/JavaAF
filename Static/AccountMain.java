deb7019695acbdd275b815ba0524e1addf49d73c
Anuj Vidhate <84793455+Anuj7th-Hokage@users.noreply.github.com>
2024-11-27 16:15:43 +0530

    Day3-Static

--- Static/AccountMain.java
+++
@@ -1,1 +0,0 @@
-package Static;public class AccountMain {    public static void main(String[]args){        Account acc=new Account();        acc.printAccount();        Account acc2=new Account(23421,"axis");        acc2.printAccount();        System.out.println(Account.getInterest());;    }}
